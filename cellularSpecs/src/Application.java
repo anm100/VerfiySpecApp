59b16decf715056cc8ca39af6d8693afe1ed7eba
DESKTOP-HGRT63B\Ahmad Mnasra <ahmad.mnasra@gmail.com>
2017-04-30 00:46:15 +0300

    new GUI design for project

--- cellularSpecs/src/Application.java
+++ cellularSpecs/src/Application.java
@@ -1,5 +1,6 @@
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 import Controller.Router;
 import Model.WorkSpace;
@@ -7,6 +8,20 @@ import ToolGUI.NewSpecGUI;
 
 public  class Application{
 	public static void main(String[] args) {
+		try {
+            //here you can put the selected theme class name in JTattoo
+            UIManager.setLookAndFeel("com.jtattoo.plaf.texture.TextureLookAndFeel");
+ 
+        } catch (ClassNotFoundException ex) {
+        //    java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+        //    java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+        //    java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            //java.util.logging.Logger.getLogger(PC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
 		NewSpecGUI newSpecGUI=new NewSpecGUI();
 		Router.setInstance(newSpecGUI);
 		newSpecGUI.addWorkSpaceListener(Router.getInstance());
